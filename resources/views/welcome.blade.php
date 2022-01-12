918a34bae050fe1130bb7608c6c97563bcaf4293
halgorn <brunoinacio3000@hotmail.com>
2022-01-12 15:38:52 -0300

    retirar opção de cadastro da home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -94,9 +94,7 @@
                         @else
                             <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Entrar</a>
 
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Registrar-se</a>
-                            @endif
+
                         @endauth
 
                 @endif
